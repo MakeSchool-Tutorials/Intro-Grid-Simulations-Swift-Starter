33ba9dfc49d081b021bd7c4ddc72fabc7e83cf61
Dion Larson <me@dionlarson.com>
2016-09-13 00:52:07 -0700

    Add GOL solution

--- Grid-Simulations/GOL/GameOfLifeSimulation.swift
+++ Grid-Simulations/GOL/GameOfLifeSimulation.swift
@@ -13,14 +13,53 @@ public class GameOfLifeSimulation: Simulation {
     public var liveChar: Character = "👾"
     
     public override func update() {
-        
+        var newGrid = grid
+        for x in 0..<grid.count {
+            for y in 0..<grid[0].count {
+                let cell = grid[x][y]
+                let neighborAliveCount = countNeighbors(grid: grid, column: x, row: y)
+                if cell == nil {
+                    // cell is dead
+                    if neighborAliveCount == 3 {
+                        // a new cell is born!
+                        newGrid[x][y] = liveChar
+                    }
+                    // don't need an else since we copied the entire grid
+                    // over to newGrid
+                } else {
+                    // cell is alive
+                    if neighborAliveCount < 2 || neighborAliveCount > 3 {
+                        // dies of over/under population
+                        newGrid[x][y] = nil
+                    }
+                    // don't need an else since we copied the entire grid
+                    // over to newGrid
+                }
+            }
+        }
+        grid = newGrid
     }
 
     func getAlive(grid: [[Character?]], column x: Int, row y: Int) -> Int {
-        return 1
+        if x < 0 || x >= grid.count || y < 0 || y >= grid[0].count {
+            return 0
+        } else if grid[x][y] == nil {
+            return 0
+        } else {
+            return 1
+        }
     }
     
     func countNeighbors(grid: [[Character?]], column x: Int, row y: Int) -> Int {
-        return 1
+        var aliveCount = 0
+        aliveCount += getAlive(grid: grid, column: x - 1, row: y - 1)
+        aliveCount += getAlive(grid: grid, column: x - 1, row: y)
+        aliveCount += getAlive(grid: grid, column: x - 1, row: y + 1)
+        aliveCount += getAlive(grid: grid, column: x, row: y - 1)
+        aliveCount += getAlive(grid: grid, column: x, row: y + 1)
+        aliveCount += getAlive(grid: grid, column: x + 1, row: y - 1)
+        aliveCount += getAlive(grid: grid, column: x + 1, row: y)
+        aliveCount += getAlive(grid: grid, column: x + 1, row: y + 1)
+        return aliveCount
     }
 }
